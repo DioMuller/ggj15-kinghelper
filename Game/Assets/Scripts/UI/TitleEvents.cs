3147df632a9fafda3bde5c207fabd5d5f20b00fe
Diogo Muller <diogo.muller@live.com>
2015-01-24 00:36:25 -0200

    Added placeholder for title screen, added main character sprite.

---
+++ Game/Assets/Scripts/UI/TitleEvents.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class TitleEvents : MonoBehaviour 
+{
+	public void OnNewGameClick()
+	{
+		
+	}
+
+	public void OnCreditsClick()
+	{
+		
+	}
+}
