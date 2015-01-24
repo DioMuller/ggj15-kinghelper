ae3e248575129f8d492ed51fbcd31762f592fc0f
Diogo Muller <diogo.muller@live.com>
2015-01-24 21:14:49 -0200

    Coins GUI and fixed audio.

--- Game/Assets/Scripts/CoinManager.cs
+++ Game/Assets/Scripts/CoinManager.cs
@@ -1,9 +1,13 @@
+using System.Net.Mime;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class CoinManager : MonoBehaviour 
 {
 	public static CoinManager Instance { get; private set; }
+
+	public Text Label;
 
 	public int Coins { get; private set; }
 
@@ -12,13 +16,13 @@ public class CoinManager : MonoBehaviour
 		Instance = this;
 	}
 
+	void Update()
+	{
+		Label.text = Coins + "/8";
+	}
+
 	public void AddCoins(int coins)
 	{
 		Coins += coins;
-	}
-
-	void OnGUI()
-	{
-		
 	}
 }
