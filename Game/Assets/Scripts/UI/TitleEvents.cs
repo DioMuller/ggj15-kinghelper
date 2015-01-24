4d8a601e4c171c806f099503270576e2b0c2b3f3
Diogo Muller <diogo.muller@live.com>
2015-01-24 01:08:22 -0200

    Basic main screen navigation.

--- Game/Assets/Scripts/UI/TitleEvents.cs
+++ Game/Assets/Scripts/UI/TitleEvents.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public class TitleEvents : MonoBehaviour 
+public class TitleEvents : MonoBehaviour
 {
+	public Canvas MainCanvas = null;
+	public Canvas CreditsCanvas = null;
+	public string GameSceneName = "";
+
 	public void OnNewGameClick()
 	{
-		
+		Application.LoadLevel(GameSceneName);
 	}
 
 	public void OnCreditsClick()
 	{
-		
+		MainCanvas.enabled = false;
+		CreditsCanvas.enabled = true;
+	}
+
+	public void OnBackClick()
+	{
+		MainCanvas.enabled = true;
+		CreditsCanvas.enabled = false;
 	}
 }
